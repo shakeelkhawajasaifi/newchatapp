973e879978cff6f3a9b2431d33ab4884e0abadf3
Shakeel Khawaja <shakeel.saifi450072@gmail.com>
2025-06-11 13:43:00 +0530

    deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectToMongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log("Connected to MongoDB Atlas ✅");
     } catch (error) {

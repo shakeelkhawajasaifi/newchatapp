11496a0e64a760761f4e95b66af9d3d86374ac2f
Shakeel Khawaja <shakeel.saifi450072@gmail.com>
2025-06-11 13:25:53 +0530

    deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB Atlas ✅");
+    } catch (error) {
+        console.log("Error connecting to MongoDB ❌", error.message);
+        process.exit(1); // Exit if database connection fails
+    }
 };
 
 export default connectToMongoDB;
